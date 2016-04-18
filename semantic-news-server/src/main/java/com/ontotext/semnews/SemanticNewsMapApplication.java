a965202ae1097007cd004922ae4a429e6bfd3c84
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2016-04-18 10:32:53 +0300

    Initial commit

---
+++ semantic-news-server/src/main/java/com/ontotext/semnews/SemanticNewsMapApplication.java
@@ -0,0 +1,25 @@
+package com.ontotext.semnews;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Tsvetan Dimitrov <tsvetan.dimitrov@ontotext.com>
+ * @since 15-Apr-2016
+ */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SemanticNewsMapApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SemanticNewsMapApplication.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(SemanticNewsMapApplication.class, args);
+    }
+}
